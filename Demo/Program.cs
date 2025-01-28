9cf5b3f213c06e35fdefeb356cfb8f9b3941821a
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-28 21:45:54 +0200

    Extension Method

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,14 +5,19 @@
         static void Main(string[] args)
         {
             #region Anomynous Type
-            //Employee employee = new Employee() { Id = 3, Name = "Omar", Salary = 30000 };
-            var employee = new { Id = 3, Name = "Omar", Salary = 30000 }; //Anomynous Object, Have Only "Get" don't have Set{Read Only}
-            Console.WriteLine(employee.GetType().Name);
-            Console.WriteLine(employee);
-
-            var employee2 = new { Id = 2, Name = "Mai", Salary = 20000, Age = 20 }; //Anomynous Object, Have Only "Get" don't have Set{Read Only} 
-            Console.WriteLine(employee2.GetType().Name);
-            Console.WriteLine(employee2);
+            ////Employee employee = new Employee() { Id = 3, Name = "Omar", Salary = 30000 };
+            //var employee = new { Id = 3, Name = "Omar", Salary = 30000 }; //Anomynous Object, Have Only "Get" don't have Set{Read Only}
+            //Console.WriteLine(employee.GetType().Name);
+            //Console.WriteLine(employee);
+            ////employee.Name = "Ali"; //Invalid Don't have Setter
+            //var employee2 = new { Id = 2, Name = "Mai", Salary = 20000, Age = 20 }; //Anomynous Object, Have Only "Get" don't have Set{Read Only} 
+            //Console.WriteLine(employee2.GetType().Name);
+            //Console.WriteLine(employee2);
+            #endregion
+            #region Extension Method
+            //int x = 12345;
+            //int y = x.Reverse();
+            //Console.WriteLine(y);
             #endregion
         }
     }
