db09b1bb72c7aae3d4b8c00573e3d4bee485acba
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-28 21:31:36 +0200

    Anomynous Type

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Anomynous Type
+            //Employee employee = new Employee() { Id = 3, Name = "Omar", Salary = 30000 };
+            var employee = new { Id = 3, Name = "Omar", Salary = 30000 }; //Anomynous Object, Have Only "Get" don't have Set{Read Only}
+            Console.WriteLine(employee.GetType().Name);
+            Console.WriteLine(employee);
+
+            var employee2 = new { Id = 2, Name = "Mai", Salary = 20000, Age = 20 }; //Anomynous Object, Have Only "Get" don't have Set{Read Only} 
+            Console.WriteLine(employee2.GetType().Name);
+            Console.WriteLine(employee2);
+            #endregion
         }
     }
 }
