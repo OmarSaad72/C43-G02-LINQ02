f903061a1148875f01050fd97c2a57101586e9fd
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-28 23:09:31 +0200

    Set Operators

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -102,7 +102,19 @@ namespace Demo
             //    Console.WriteLine(item);
             //}
             //Empty:
-            var result = Enumerable.Empty<Product>();
+            //var result = Enumerable.Empty<Product>();
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region Set Operators
+            var Seq01 = Enumerable.Range(0, 100);
+            var Seq02 = Enumerable.Range(50, 100);
+            //var result= Seq01.Union(Seq02);
+            //var result= Seq01.Concat(Seq02);
+            //var result= Seq01.Intersect(Seq02);
+            var result= Seq01.Except(Seq02);
             foreach (var item in result)
             {
                 Console.WriteLine(item);
